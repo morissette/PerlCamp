3af3c48f21ce081ba50081a258c37ab3f8a7e299
Matt Harris <admin@mattharris.org>
2016-01-06 22:07:46 +0000

    Beginning of filter Added perl glossary Angular routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,63 @@
 /* 
  * Init angular app
  */
-    var app = angular.module('perlCamp', []);
+    var app = angular.module('perlCamp', ['ngRoute']);
+
+    /*
+     * Handle Routing
+     */
+    app.config(function($routeProvider) {
+	$routeProvider
+            .when('/', {
+                templateUrl: 'tmpl/welcome.html',
+            })
+
+	    .when('/glossary', {
+                templateUrl: 'tmpl/glossary.html',
+		controller: 'glossaryCtrl',
+            })
+    })
 
     app.controller('mainController', function($scope) {
 
     })
 
+    app.controller('glossaryCtrl', function($scope) {
+        
+    })
+
+    app.directive('filterGlossary', function($timeout) {
+        return {
+            link: function(scope, element, attrs) {
+                var li = Array.prototype.slice.call(element[0].children);
+
+                function filterBy(value) {
+                    li.forEach(function(el) {
+                        el.className = el.textContent.toLowerCase().indexOf(value.toLowerCase()) !== -1 ? '' : 'ng-hide';
+                    });
+                }
+
+                scope.$watch(attrs.filterList, function(newVal, oldVal) {
+                    if ( newVal !== oldVal ) {
+                        filterBy(newVal);
+                    }
+                });
+            }
+        };
+    })
+
     app.directive('welcomePage', function() {
-      return {
-        restrict: 'E',
-        templateUrl: 'tmpl/welcome.html',
-      };
+        return {
+            restrict: 'E',
+            templateUrl: 'tmpl/welcome.html',
+        };
+    })
+
+    app.directive('glossaryPage', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'tmpl/glossary.html',
+        };
     });
 
 })();
